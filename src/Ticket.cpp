2076be13f44aa5a938a0381e0986d7be7bd81522
raluca <ralucatoma@yahoo.com>
2025-05-23 19:28:37 +0300

    changed tastatura.txt - 2

---
+++ src/Ticket.cpp
@@ -0,0 +1,8 @@
+// #include "../include/Ticket.h"
+//
+// template <>
+// double Ticket<Visitor, Exhibition>::moneySpent = 0;
+//
+//
+// template <>
+// double Ticket<VipVisitor, VipExhibitionEvent>::moneySpent = 0;
