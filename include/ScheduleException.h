2a88f7f8827ea828178885b17c47e4eb15845612
raluca <ralucatoma@yahoo.com>
2025-05-25 14:52:41 +0300

    changed main file

--- include/ScheduleException.h
+++ include/ScheduleException.h
@@ -1,22 +1,29 @@
-//// ExhibitionException.h
-//#ifndef EXHIBITIONEXCEPTION_H
-//#define EXHIBITIONEXCEPTION_H
-//
-//#include <exception>
-//#include <string>
-//
-//class ExhibitionException : public std::exception {
-//    std::string message;
-//public:
-//    explicit ExhibitionException(const std::string& msg = "Exhibition error") : message(msg) {}
-//    const char* what() const noexcept override {
-//        return message.c_str();
-//    }
-//};
-//
-//class InvalidMenuChoiceException : public ExhibitionException {
-//public:
-//    InvalidMenuChoiceException() : ExhibitionException("Invalid choice. Please enter 'login' or 'exit'.") {}
-//};
-//
-//#endif // EXHIBITIONEXCEPTION_H
+// ExhibitionException.h
+#ifndef EXHIBITIONEXCEPTION_H
+#define EXHIBITIONEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class ExhibitionException : public std::exception {
+    std::string message;
+public:
+    explicit ExhibitionException(const std::string& msg = "Exhibition error") : message(msg) {}
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+};
+
+class InvalidCityException : public ExhibitionException {
+public:
+    InvalidCityException() : ExhibitionException("Unsupported city. Please enter a valid city.") {}
+};
+
+class InvalidExhibitionTypeException : public ExhibitionException {
+public:
+    InvalidExhibitionTypeException() : ExhibitionException("Unsupported exhibition type. Choose art/history/science/vip.") {}
+};
+
+
+
+#endif // EXHIBITIONEXCEPTION_H
--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@
 #include "include/TourGuide.h"
 #include "include/Cashier.h"
 #include "include/Menu.h"
+#include "include/ScheduleException.h"
 
 
 int main() {
@@ -32,6 +33,37 @@ int main() {
     std::vector<std::shared_ptr<Ticket<Visitor, Exhibition>>> tickets;
     std::vector<std::shared_ptr<Ticket<VipVisitor, VipExhibitionEvent>>> vipTickets;
     menu.seedData(exhibitions, visitors, employees, vipTickets);
+    std::string city;
+    while (true) {
+        try {
+            std::cout << "Enter the city where you'd like to explore exhibitions (e.g., Bucharest, Cluj): ";
+            std::getline(std::cin, city);
+            if (city != "Bucharest" && city != "Cluj") {
+                throw InvalidCityException();
+            }
+            break;
+        }
+        catch (const ExhibitionException& e) {
+            std::cerr << "Error: " << e.what() << "\nPlease try again.\n";
+        }
+    }
+
+    std::string type;
+    while (true) {
+        try {
+            std::cout << "What type of exhibition are you interested in? (art/history/science/vip): ";
+            std::getline(std::cin, type);
+            if (type != "art" && type != "history" && type != "science" && type != "vip") {
+                throw InvalidExhibitionTypeException();
+            }
+            break;
+        }
+        catch (const ExhibitionException& e) {
+            std::cerr << "Error: " << e.what() << "\nPlease try again.\n";
+        }
+    }
+
+    std::cout << "Great! Let's continue to login.\n\n";
     bool exitApp = false;
     while (!exitApp) {
         bool gamePlayed = false;
